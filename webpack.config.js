09be2d6484d0c2655f3c5008da83e0d6be53fea4
Fauzi Aulia <fuzzy@gmail.com>
2017-09-01 14:26:04 +0000

    add youtube search simple app

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,14 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
+  watchOptions: {
+      aggregateTimeout: 300,
+      poll: 300
+  },
   devServer: {
     historyApiFallback: true,
-    contentBase: './'
+    contentBase: './',
+    host: "0.0.0.0",
+    port: 80
   }
 };
