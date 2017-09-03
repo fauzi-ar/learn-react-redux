dfc6d133a3ad904128d5d2418ad0d45676a6d606
Fauzi Aulia <fuzzy@gmail.com>
2017-09-03 14:08:49 +0000

    redux init

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{title: 'The Road', pages: 359},
+		{title: 'American Gods', pages: 700},
+		{title: 'Sophie\'s World', pages: 621},
+		{title: 'Supernova', pages: 278}
+	]
+}
